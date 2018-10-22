4c282e69db69c3c069a306bb4f06cb26ca164e3d
Aditya Lala <alala@latedays.andrew.cmu.edu>
2018-10-22 18:31:50 -0400

    first commit

---
+++ wire_route.h
@@ -0,0 +1,28 @@
+#ifndef _WIRE_ROUTE_H
+#define _WIRE_ROUTE_H
+#define undef -1
+
+typedef struct wire_t{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int b1x;
+    int b1y;
+    int b2x;
+    int b2y;
+}wire_t;
+
+// Perform computation, including reading/writing output files
+void compute(int procID, int nproc, char* inputFilename, double prob, int numIterations);
+
+// Read input file
+wire_t *readInput(char* inputFilename,int procID,int nProcs,int *numRows,int *numCols,int *delta,int *wiresPerProc);
+
+// Write cost array file based on input filename
+void writeCost(char* inputFilename, int nproc);
+
+// Write wire output file based on input filename
+void writeOutput(char* inputFilename, int nproc);
+
+#endif // _WIRE_ROUTE_H
